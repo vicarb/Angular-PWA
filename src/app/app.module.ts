be18e60eaa5ec7a2f78a34f225ffe9ebfd19828f
vicarb <vcardenasb@protonmail.com>
2022-09-25 20:20:29 -0300

    added ngrx state management

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,25 @@ import { AppComponent } from './app.component';
 import { ContainerComponent } from './Container/container.component';
 import { NavComponent } from './nav/nav.component';
 import { HeaderComponent } from './header/header.component';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { MyCounterComponent } from './my-counter/my-counter.component';
+import { counterReducer } from './state/Counter/counter.reducer';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContainerComponent,
     NavComponent,
-    HeaderComponent
+    HeaderComponent,
+    MyCounterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({count: counterReducer }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/my-counter/my-counter.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { increment, decrement, reset } from '../state/Counter/counter.action';
+@Component({
+  selector: 'app-my-counter',
+  templateUrl: './my-counter.component.html',
+  styleUrls: ['./my-counter.component.css']
+})
+export class MyCounterComponent implements OnInit {
+
+  count$: Observable<number>;
+  constructor(private store: Store<{ count: number }>) {
+    this.count$ = store.select('count');
+  }
+  //Implement the increment, decrement, and reset methods by dispatching actions to the store.
+  increment() {
+    this.store.dispatch(increment());
+  }
+  decrement() {
+    this.store.dispatch(decrement());
+  }
+  reset() {
+    this.store.dispatch(reset());
+  }
+
+  ngOnInit(): void {
+  }
+
+}
