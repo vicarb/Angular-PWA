f56a7995e2e11f99b7c68749b2d0dd80c661b7d3
vicarb <vcardenasb@protonmail.com>
2022-09-25 19:54:55 -0300

    ng components

---
+++ src/app/Container/container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:`app-container`,
+    templateUrl: `./container.component.html`
+})
+
+export class ContainerComponent {
+
+}
